61fdcb23b7898064b88bfc8dfd4ab9e55d499fe5
vanansa <saravananr6332@gmail.com>
2020-12-19 20:22:05 +0530

    have done the array testing

--- array_test.sh
+++ array_test.sh
@@ -1,19 +1,27 @@
 #!/bin/bash 
-set -x
-
+#set -x
 tables=('tb1' 'tb2' 'tb3' 'tb4' 'tb5')
-
 coa=${#tables[*]}
+#echo $coa
 
-
-echo $coa
-
-
-
-
-
-for i in $(  )
-do 
-	echo $i;
+i=0
+get_tables () {
+while [ $i -lt $coa ]
+do
+	echo ${tables[$i]};
+	#sleep 2
+	(( i++ ))
 done
+}
+
+
+PS3="Enter any below option "
+
+
+
+
+
+
+
+
 
